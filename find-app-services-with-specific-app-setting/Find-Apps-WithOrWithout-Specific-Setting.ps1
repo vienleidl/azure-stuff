e9f698b4d3f55adf6e76619fb325195fc654e626
Vien Le <vien.le@publishone.nl>
2024-12-13 13:49:52 +0700

    Enhance Script to Identify App Services with Flexible App Settings
    This update enhances the existing script to identify Azure App Services and Function Apps that either have or lack specific application settings. The key improvement includes:
    
    Flexible Application Settings: The script now supports checking for one or two application settings. If the second setting key (AppSettingKeyName2) is left blank, the script will only check for the first setting key (AppSettingKeyName1).

--- find-app-services-with-specific-app-setting/Find-Apps-WithOrWithout-Specific-Setting.ps1
+++ find-app-services-with-specific-app-setting/Find-Apps-WithOrWithout-Specific-Setting.ps1
@@ -1,16 +1,19 @@
 <#
 .SYNOPSIS
-    This script identifies Azure App Services and Function Apps that have or do not have a specific application setting.
+    Identifies Azure App Services and Function Apps that either have or lack specific application settings.
 
 .DESCRIPTION
-    The script connects to an Azure account, retrieves all App Services and Function Apps in the specified subscription, and checks each one for a specified application setting. 
-    It displays the names of those that match the search criteria in a grid view. The script supports parallel processing to speed up the checking process.
+    This script connects to an Azure account, retrieves all App Services and Function Apps within a specified subscription, and checks each one for the presence of specified application settings. 
+    It displays the names of those that match the search criteria in a grid view. The script supports parallel processing to enhance performance.
 
-.PARAMETER AppSettingKeyName
-    The key of the application setting to search for. Defaults to "YOUR_SETTING_KEY_NAME".
+.PARAMETER AppSettingKeyName1
+    The first application setting key to search for. Defaults to "YOUR_SETTING_KEY_NAME1".
+
+.PARAMETER AppSettingKeyName2
+    The second application setting key to search for. If left blank, the script will only check for AppSettingKeyName1.
 
 .PARAMETER SearchType
-    Specifies whether to search for App Services with or without the specified setting key. Valid values are "With" and "Without".
+    Specifies whether to search for App Services with or without the specified setting keys. Valid values are "With" and "Without".
 
 .PARAMETER FilterByTag
     Specifies whether to filter App Services by specific tags. If set to $true, only App Services with the specified tags and values will be included.
@@ -31,11 +34,12 @@
     Ensure the Azure PowerShell module is installed and you are connected to your Azure account before running this script.
 
 .EXAMPLE
-    .\Find-Apps-WithOrWithout-Specific-Setting.ps1 -AppSettingKeyName "YOUR_CUSTOM_SETTING_KEY" -SearchType "With" -FilterByTag $true -TagName1 "landscape" -TagValue1 "production"
+    .\Find-Apps-WithOrWithout-Specific-Setting.ps1 -AppSettingKeyName1 "YOUR_CUSTOM_SETTING_KEY1" -AppSettingKeyName2 "YOUR_CUSTOM_SETTING_KEY2" -SearchType "With" -FilterByTag $true -TagName1 "landscape" -TagValue1 "production"
 #>
 
 param (
-    [string]$AppSettingKeyName = "YOUR_SETTING_KEY_NAME",
+    [string]$AppSettingKeyName1 = "YOUR_SETTING_KEY_NAME1",
+    [string]$AppSettingKeyName2 = "YOUR_SETTING_KEY_NAME2", # Optional second application setting key to search for
     [ValidateSet("With", "Without")]
     [string]$SearchType        = "With",
     [bool]$FilterByTag         = $false,
@@ -47,7 +51,8 @@ param (
 
 function Find-AppServices {
     param (
-        [string]$AppSettingKeyName,
+        [string]$AppSettingKeyName1,
+        [string]$AppSettingKeyName2,
         [string]$SearchType,
         [bool]$FilterByTag,
         [string]$TagName1,
@@ -92,27 +97,36 @@ function Find-AppServices {
         $appList = Get-AzWebApp 2>$null | Select-Object Name, ResourceGroup | Sort-Object -Property Name
     }
 
-    Write-Host -ForegroundColor Green ("The names of apps which " + ($SearchType -eq "With" ? "have" : "do not have") + " the [" + $AppSettingKeyName + "] setting are being collected...")
+    Write-Host -ForegroundColor Green ("The names of apps which " + ($SearchType -eq "With" ? "have" : "do not have") + " the [" + $AppSettingKeyName1 + "]" + ($AppSettingKeyName2 -ne "" ? " and [" + $AppSettingKeyName2 + "]" : "") + " setting(s) are being collected...")
 
     $appServicesResult = @()
     $jobs = @()
 
     foreach ($app in $appList) {
         $jobs += Start-Job -ScriptBlock {
-            param ($app, $AppSettingKeyName, $SearchType)
+            param ($app, $AppSettingKeyName1, $AppSettingKeyName2, $SearchType)
 
             $appSettings = (Get-AzWebApp -ResourceGroupName $app.ResourceGroup -Name $app.Name -WarningAction SilentlyContinue).SiteConfig.AppSettings
 
-            $hasSetting = $false
+            $hasSetting1 = $false
+            $hasSetting2 = $false
             foreach ($setting in $appSettings) {
-                if ($setting.Name -eq $AppSettingKeyName) {
-                    $hasSetting = $true
-                    break
+                if ($setting.Name -eq $AppSettingKeyName1) {
+                    $hasSetting1 = $true
+                }
+                if ($AppSettingKeyName2 -ne "" -and $setting.Name -eq $AppSettingKeyName2) {
+                    $hasSetting2 = $true
                 }
             }
 
-            if (($SearchType -eq "With" -and $hasSetting) -or ($SearchType -eq "Without" -and -not $hasSetting)) {
-                return [PSCustomObject]@{ Name = $app.Name; ResourceGroup = $app.ResourceGroup }
+            if ($AppSettingKeyName2 -eq "") {
+                if (($SearchType -eq "With" -and $hasSetting1) -or ($SearchType -eq "Without" -and -not $hasSetting1)) {
+                    return [PSCustomObject]@{ Name = $app.Name; ResourceGroup = $app.ResourceGroup }
+                }
+            } else {
+                if (($SearchType -eq "With" -and $hasSetting1 -and $hasSetting2) -or ($SearchType -eq "Without" -and -not $hasSetting1 -and -not $hasSetting2)) {
+                    return [PSCustomObject]@{ Name = $app.Name; ResourceGroup = $app.ResourceGroup }
+                }
             }
 
             $appSlots = Get-AzWebAppSlot -ResourceGroupName $app.ResourceGroup -Name $app.Name -WarningAction SilentlyContinue
@@ -123,20 +137,29 @@ function Find-AppServices {
                     $fullSlotName = Get-AzWebAppSlot -ResourceGroupName $appSlot.ResourceGroup -Name $app.Name -Slot $slotName -WarningAction SilentlyContinue
                     $slotSettings = ($fullSlotName).SiteConfig.AppSettings
 
-                    $slotHasSetting = $false
+                    $slotHasSetting1 = $false
+                    $slotHasSetting2 = $false
                     foreach ($setting in $slotSettings) {
-                        if ($setting.Name -eq $AppSettingKeyName) {
-                            $slotHasSetting = $true
-                            break
+                        if ($setting.Name -eq $AppSettingKeyName1) {
+                            $slotHasSetting1 = $true
+                        }
+                        if ($AppSettingKeyName2 -ne "" -and $setting.Name -eq $AppSettingKeyName2) {
+                            $slotHasSetting2 = $true
                         }
                     }
 
-                    if (($SearchType -eq "With" -and $slotHasSetting) -or ($SearchType -eq "Without" -and -not $slotHasSetting)) {
-                        return [PSCustomObject]@{ Name = $appSlot.Name; ResourceGroup = $appSlot.ResourceGroup }
+                    if ($AppSettingKeyName2 -eq "") {
+                        if (($SearchType -eq "With" -and $slotHasSetting1) -or ($SearchType -eq "Without" -and -not $slotHasSetting1)) {
+                            return [PSCustomObject]@{ Name = $appSlot.Name; ResourceGroup = $appSlot.ResourceGroup }
+                        }
+                    } else {
+                        if (($SearchType -eq "With" -and $slotHasSetting1 -and $slotHasSetting2) -or ($SearchType -eq "Without" -and -not $slotHasSetting1 -and -not $slotHasSetting2)) {
+                            return [PSCustomObject]@{ Name = $appSlot.Name; ResourceGroup = $appSlot.ResourceGroup }
+                        }
                     }
                 }
             }
-        } -ArgumentList $app, $AppSettingKeyName, $SearchType
+        } -ArgumentList $app, $AppSettingKeyName1, $AppSettingKeyName2, $SearchType
     }
 
     # Display a message indicating the script is processing
@@ -159,11 +182,11 @@ function Find-AppServices {
 
     # Output the App Services based on the search type
     if ($appServicesResult.Count -gt 0) {
-        $appServicesResult | Sort-Object Name | Out-GridView -Title ("App Services " + ($SearchType -eq "With" ? "with" : "without") + " the setting '$AppSettingKeyName'")
+        $appServicesResult | Sort-Object Name | Out-GridView -Title ("App Services " + ($SearchType -eq "With" ? "with" : "without") + " the setting(s) '$AppSettingKeyName1'" + ($AppSettingKeyName2 -ne "" ? " and '$AppSettingKeyName2'" : ""))
     } else {
-        Write-Output ("No App Services found " + ($SearchType -eq "With" ? "with" : "without") + " the setting '$AppSettingKeyName'.")
+        Write-Output ("No App Services found " + ($SearchType -eq "With" ? "with" : "without") + " the setting(s) '$AppSettingKeyName1'" + ($AppSettingKeyName2 -ne "" ? " and '$AppSettingKeyName2'" : ""))
     }
 }
 
 # Call the function
-Find-AppServices -AppSettingKeyName $AppSettingKeyName -SearchType $SearchType -FilterByTag $FilterByTag -TagName1 $TagName1 -TagValue1 $TagValue1 -TagName2 $TagName2 -TagValue2 $TagValue2
+Find-AppServices -AppSettingKeyName1 $AppSettingKeyName1 -AppSettingKeyName2 $AppSettingKeyName2 -SearchType $SearchType -FilterByTag $FilterByTag -TagName1 $TagName1 -TagValue1 $TagValue1 -TagName2 $TagName2 -TagValue2 $TagValue2
